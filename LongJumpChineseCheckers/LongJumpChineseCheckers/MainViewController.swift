90e1bc51cb50e1c223112b6f9b68050e10f3baad
escobalj <escobalj@sou.edu>
2017-05-25 15:30:50 -0700

    Add files via upload

--- LongJumpChineseCheckers/LongJumpChineseCheckers/MainViewController.swift
+++ LongJumpChineseCheckers/LongJumpChineseCheckers/MainViewController.swift
@@ -10,50 +10,94 @@ import UIKit
 
 class MainViewController: UIViewController
 {
-    @IBOutlet weak var b1: UIButton!
-    @IBOutlet weak var b2: UIButton!
-    @IBOutlet weak var b3: UIButton!
-    @IBOutlet weak var b4: UIButton!
+    // Outlets:
+    @IBOutlet weak var twoPlayersButton: UIButton!
+    @IBOutlet weak var threePlayersButton: UIButton!
+    @IBOutlet weak var fourPlayersButton: UIButton!
+    @IBOutlet weak var sixPlayersButton: UIButton!
     @IBOutlet weak var tutorialButton: UIButton!
-    @IBOutlet weak var v1: UIView!
+    @IBOutlet weak var menuView: UIView!
     
+    
+    // Target-actions:
+    @IBAction func showTwoPlayersBoard(_ sender: UIButton)
+    {
+        // Perform the segue with the indicated identifier.
+        performSegue(withIdentifier: "ShowBoardSegue", sender: self)
+    }
+    
+    @IBAction func showThreePlayersBoard(_ sender: UIButton)
+    {
+        // Perform the segue with the indicated identifier.
+        performSegue(withIdentifier: "ShowBoardSegue", sender: self)
+    }
+    
+    @IBAction func showFourPlayersBoard(_ sender: UIButton)
+    {
+        // Perform the segue with the indicated identifier.
+        performSegue(withIdentifier: "ShowBoardSegue", sender: self)
+    }
+    
+    @IBAction func showSixPlayersBoard(_ sender: UIButton)
+    {
+        // Perform the segue with the indicated identifier.
+        performSegue(withIdentifier: "ShowBoardSegue", sender: self)
+    }
+    
+    @IBAction func showTutorialScreen(_ sender: UIButton)
+    {
+        // Perform the segue with the indicated identifier.
+        performSegue(withIdentifier: "ShowTutorialSegue", sender: self)
+    }
+    
+    
+    // UIViewController Protocol Methods:
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
-        // ViewController Background
+        // ViewController Background:
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background1.jpg")!)
         
-        // Button Styles
-        b1.layer.cornerRadius = 20
-        b1.layer.borderColor = UIColor.black.cgColor
-        b1.layer.borderWidth = 3
+        // Button Layers:
+        twoPlayersButton.layer.cornerRadius = 20
+        twoPlayersButton.layer.borderColor = UIColor.black.cgColor
+        twoPlayersButton.layer.borderWidth = 3
         
-        b2.layer.cornerRadius = 20
-        b2.layer.borderColor = UIColor.black.cgColor
-        b2.layer.borderWidth = 3
+        threePlayersButton.layer.cornerRadius = 20
+        threePlayersButton.layer.borderColor = UIColor.black.cgColor
+        threePlayersButton.layer.borderWidth = 3
         
-        b3.layer.cornerRadius = 20
-        b3.layer.borderColor = UIColor.black.cgColor
-        b3.layer.borderWidth = 3
+        fourPlayersButton.layer.cornerRadius = 20
+        fourPlayersButton.layer.borderColor = UIColor.black.cgColor
+        fourPlayersButton.layer.borderWidth = 3
         
-        b4.layer.cornerRadius = 20
-        b4.layer.borderColor = UIColor.black.cgColor
-        b4.layer.borderWidth = 3
+        sixPlayersButton.layer.cornerRadius = 20
+        sixPlayersButton.layer.borderColor = UIColor.black.cgColor
+        sixPlayersButton.layer.borderWidth = 3
         
         tutorialButton.layer.cornerRadius = 20
         tutorialButton.layer.borderColor = UIColor.black.cgColor
         tutorialButton.layer.borderWidth = 3
         
-        // View
-        v1.layer.cornerRadius = 20
-        v1.layer.borderColor = UIColor.black.cgColor
-        v1.layer.borderWidth = 3
+        // View Layers:
+        menuView.layer.cornerRadius = 20
+        menuView.layer.borderColor = UIColor.black.cgColor
+        menuView.layer.borderWidth = 3
+        menuView.layer.opacity = 0.8
     }
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue:UIStoryboardSegue, sender: Any?) // Prepare to perform a segue.
+    {
+        if let destinationVC : TutorialViewController = segue.destination as? TutorialViewController
+        {
+            destinationVC.inGame = false
+        }
     }
 }
